4e6537af6ec41fa0bb3477bfdba2174dea13aca4
sonchaegeon <thscorjs1002@naver.com>
2021-02-02 20:37:37 +0900

    UPDATE; request object
    or @param(key?: string)

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
     @Get()
-    findAll(): string {
-        return 'This action returns all cats';
+    findAll(@Req() request: Request): string {
+        return `This action returns all cats
+            <br> query: ${request.query.name}`;
     }
 }
