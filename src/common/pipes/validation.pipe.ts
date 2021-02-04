d64cdd89f4c82b43a1502bed10ef2370a75604f5
sonchaegeon <thscorjs1002@naver.com>
2021-02-04 18:48:07 +0900

    UPDATE; validation pipe
    class-transformer, class-validator

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -4,17 +4,25 @@ import {
   Injectable,
   PipeTransform,
 } from '@nestjs/common';
-import { ObjectSchema } from 'joi';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 @Injectable()
-export class ValidationPipe implements PipeTransform {
-  constructor(private schema: ObjectSchema) {}
-
-  transform(value: any, metadata: ArgumentMetadata) {
-    const { error } = this.schema.validate(value);
-    if (error) {
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
       throw new BadRequestException('Validation failed');
     }
     return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
   }
 }
